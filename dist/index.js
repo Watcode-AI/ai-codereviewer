c15eb37434fc5c1d3b14acddb3ffa9f79eb4450e
Lucas Khan <43745720+1-mbps@users.noreply.github.com>
2025-01-08 02:50:42 -0500

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -29,6 +29,17 @@ require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
         __setModuleDefault(result, mod);
         return result;
     };
+    var __assign = (this && this.__assign) || function () {
+        __assign = Object.assign || function(t) {
+            for (var s, i = 1, n = arguments.length; i < n; i++) {
+                s = arguments[i];
+                for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                    t[p] = s[p];
+            }
+            return t;
+        };
+        return __assign.apply(this, arguments);
+    };
     var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
         function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
         return new (P || (P = Promise))(function (resolve, reject) {
