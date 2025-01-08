4ea54db66f20d80d519652a672e0c4f0ebf7d9ac
Lucas Khan <43745720+1-mbps@users.noreply.github.com>
2025-01-07 23:17:11 -0500

    updated response instructions

--- src/main.ts
+++ src/main.ts
@@ -80,13 +80,11 @@ async function analyzeCode(
 }
 
 function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
-  let RESPONSE_FORMAT: string = `Instructions:
+  let RESPONSE_FORMAT: string = `Response Instructions:
 - Provide the response in following JSON format:  {"reviews": [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]}
-- Do not give positive comments or compliments.
-- Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
+- Provide comments and suggestions ONLY if there are errors, otherwise "reviews" should be an empty array.
 - Write the comment in GitHub Markdown format.
 - Use the given description only for the overall context and only comment the code.
-- IMPORTANT: NEVER suggest adding comments to the code.
 
 Review the following code diff in the file "${
     file.to
